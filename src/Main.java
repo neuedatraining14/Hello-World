4eb150df42d5cf15dc03d2b9b48f62816295bdd7
Sonya <sbrs6@allstate.com>
2022-07-26 15:26:26 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i =7;
+        System.out.println(i);
+        int j;
+        System.out.println(j);
+        j = 19;
+        long myVariable =176L;
+        long anotherVariable =9000000000L;
+        int first = 50;
+        int second = 100;
+        int third = first + 10;
+        System.out.println(third);
+        first = 65;
+        System.out.println(first);
+        System.out.println(third);
+        //printout the value of the first multiplied by 3
+        System.out.println("first+3 ="+first *3);
+        System.out.println(second /first);//1.85
+        System.out.println(first * 3.1);
+        double size = 17.3;
+        float smallersize = 17.1f;
+        double largesize = 18;
+        int extralagresize = 19;
+        extralagresize = extralagresize +1;
+        extralagresize ++;
+        System.out.println(extralagresize);
+        extralagresize --;
+        System.out.println(extralagresize);
+    }
+
+}
---
+++ src/loop.java
@@ -0,0 +1,11 @@
+
+public class loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+            int [] samplearray= new int[50];
+            int []  selectednumbers = {1,2,8,7,6,4,3,9,10,3};
+
+        }
+    }
+}
